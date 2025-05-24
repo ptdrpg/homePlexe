6c4691fdbed83a0e5131c352f01550511f7f1262
ptdrpg <pagielamizael@gmail.com>
2025-05-24 10:09:37 +0300

    fix(movies): update scanDir

--- lib/getMoviesPath.go
+++ lib/getMoviesPath.go
@@ -1,21 +1,17 @@
 package lib
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
+	"regexp"
+	"strconv"
 	"strings"
 	"sync"
 )
 
-var wg sync.WaitGroup
-var videoPath []Episode
-var allTitle []string
-var mu sync.Mutex
-
 type Serie struct {
-	Title string
-	Ep_list []string
+	Title   string   `json:"title"`
+	Ep_list []string `json:"ep_list"`
 }
 
 type Response struct {
@@ -27,6 +23,229 @@ type Episode struct {
 	Path  string
 }
 
+var videoExtensions = []string{".mp4", ".avi"}
+
+var ignoredPathPatterns = []string{
+	"/go/pkg/mod/",
+	"/go.bak/pkg/mod/",
+	"/Android/Sdk/",
+}
+
+func IsVideoFile(filename string) bool {
+	ext := strings.ToLower(filepath.Ext(filename))
+	for _, valid := range videoExtensions {
+		if ext == valid {
+			return true
+		}
+	}
+	return false
+}
+
+func removeExtension(name string) string {
+	return strings.TrimSuffix(name, filepath.Ext(name))
+}
+
+func shouldIgnoreDir(name string) bool {
+	ignored := []string{"proc", "dev", "sys", "run", "var", "etc", "boot", "tmp", "snap", "lib", "SDK"}
+	base := filepath.Base(name)
+	if strings.HasPrefix(base, ".") {
+		return true
+	}
+	for _, ig := range ignored {
+		if base == ig {
+			return true
+		}
+	}
+	return false
+}
+
+func shouldIgnorePath(path string) bool {
+	for _, pat := range ignoredPathPatterns {
+		if strings.Contains(path, pat) {
+			return true
+		}
+	}
+	return false
+}
+
+func extractSeriesName(filename string) string {
+	name := removeExtension(filepath.Base(filename))
+
+	name = strings.ReplaceAll(name, "_", " ")
+	name = strings.ReplaceAll(name, ".", " ")
+
+	re := regexp.MustCompile(`(?i)^(.*?)(?:\s*S\d{1,2}E\d{1,2}|\s*E\d{1,2}|\s*Episode\s*\d+|\s*\d{1,3})(?:\s|$)`)
+	matches := re.FindStringSubmatch(name)
+	if len(matches) > 1 {
+		seriesName := strings.TrimSpace(matches[1])
+		return seriesName
+	}
+
+	reEndNum := regexp.MustCompile(`^(.*?)(?:\s*\d+)?$`)
+	m2 := reEndNum.FindStringSubmatch(name)
+	if len(m2) > 1 {
+		return strings.TrimSpace(m2[1])
+	}
+
+	return strings.TrimSpace(name)
+}
+
+func isSeries(files []string) bool {
+	if len(files) < 2 {
+		return false
+	}
+
+	seriesNames := make(map[string]int)
+	numbersBySeries := make(map[string][]int)
+
+	numRegexp := regexp.MustCompile(`(?i)(?:S\d{1,2}E(\d{1,2})|E(\d{1,2})|Episode\s*(\d+)|(\d+))`)
+
+	allAreNumeric := true 
+
+	for _, f := range files {
+		base := extractSeriesName(f)
+		seriesNames[base]++
+
+		name := removeExtension(filepath.Base(f))
+		numMatches := numRegexp.FindStringSubmatch(name)
+
+		episodeNum := -1
+		if numMatches != nil {
+			for _, match := range numMatches[1:] {
+				if match != "" {
+					episodeNum = atoiSafe(match)
+					break
+				}
+			}
+		} else {
+			if onlyNumber := regexp.MustCompile(`^\d+$`).MatchString(name); onlyNumber {
+				episodeNum = atoiSafe(name)
+			} else {
+				allAreNumeric = false
+			}
+		}
+
+		numbersBySeries[base] = append(numbersBySeries[base], episodeNum)
+	}
+
+	if allAreNumeric {
+		return true
+	}
+
+	var mainSeries string
+	maxCount := 0
+	for s, c := range seriesNames {
+		if c > maxCount {
+			maxCount = c
+			mainSeries = s
+		}
+	}
+
+	nums := numbersBySeries[mainSeries]
+	validNums := []int{}
+	for _, n := range nums {
+		if n >= 0 {
+			validNums = append(validNums, n)
+		}
+	}
+
+	if len(validNums) < 2 {
+		return false
+	}
+
+	for i := 0; i < len(validNums)-1; i++ {
+		for j := i + 1; j < len(validNums); j++ {
+			if validNums[i] > validNums[j] {
+				validNums[i], validNums[j] = validNums[j], validNums[i]
+			}
+		}
+	}
+
+	for i := 1; i < len(validNums); i++ {
+		if validNums[i] != validNums[i-1]+1 {
+			return false
+		}
+	}
+
+	return true
+}
+
+
+func atoiSafe(s string) int {
+	i, _ := strconv.Atoi(s)
+	return i
+}
+
+func walkOptimized(root string, episodes chan<- Episode, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	if shouldIgnorePath(root) {
+		return
+	}
+
+	entries, err := os.ReadDir(root)
+	if err != nil {
+		return
+	}
+
+	var videoFiles []string
+	var hasSubDir bool
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			if shouldIgnoreDir(entry.Name()) {
+				continue
+			}
+			hasSubDir = true
+		} else if IsVideoFile(filepath.Join(root, entry.Name())) {
+			videoFiles = append(videoFiles, filepath.Join(root, entry.Name()))
+		}
+	}
+
+	if hasSubDir && len(videoFiles) > 0 {
+		for _, f := range videoFiles {
+			title := extractSeriesName(f)
+			if title == "" {
+				title = removeExtension(filepath.Base(f))
+			}
+			episodes <- Episode{Title: title, Path: f}
+		}
+
+		for _, entry := range entries {
+			if entry.IsDir() && !shouldIgnoreDir(entry.Name()) {
+				wg.Add(1)
+				go walkOptimized(filepath.Join(root, entry.Name()), episodes, wg)
+			}
+		}
+		return
+	}
+
+	if len(videoFiles) > 0 {
+		if isSeries(videoFiles) {
+			title := filepath.Base(root)
+			for _, f := range videoFiles {
+				episodes <- Episode{Title: title, Path: f}
+			}
+		} else {
+			for _, f := range videoFiles {
+				title := extractSeriesName(f)
+				if title == "" {
+					title = removeExtension(filepath.Base(f))
+				}
+				episodes <- Episode{Title: title, Path: f}
+			}
+		}
+		return
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() && !shouldIgnoreDir(entry.Name()) {
+			wg.Add(1)
+			go walkOptimized(filepath.Join(root, entry.Name()), episodes, wg)
+		}
+	}
+}
+
 func GetHomeDir() string {
 	home, err := os.UserHomeDir()
 	if err != nil {
@@ -35,103 +254,35 @@ func GetHomeDir() string {
 	return home
 }
 
-var videoExtensions = []string{".mp4", ".avi"}
-
-func IsVideoFile(filename string) bool {
-	for _, ext := range videoExtensions {
-		if strings.HasSuffix(strings.ToLower(filename), ext) {
-			return true
-		}
-	}
-	return false
-}
-
-func scanDir(path string, jobs chan Episode) {
-	defer wg.Done()
-	var movie Episode
-
-	entries, err := os.ReadDir(path)
-	if err != nil {
-		return
-	}
-
-	for _, entry := range entries {
-		fullpath := filepath.Join(path, entry.Name())
-		splitedPath := strings.Split(fullpath, "/")
-
-		if entry.IsDir() {
-			wg.Add(1)
-			var is_there = false
-			for _, title := range allTitle {
-				if title == splitedPath[4] {
-					is_there = true
-				}
-			}
-			if !is_there {
-				allTitle = append(allTitle, splitedPath[4])
-			}
-			go scanDir(fullpath, jobs)
-		} else if IsVideoFile(fullpath) {
-			var is_there = false
-			for _, title := range allTitle {
-				if title == splitedPath[4] {
-					is_there = true
-					movie.Title = splitedPath[4]
-					movie.Path = fullpath
-					jobs <- movie
-				}
-			}
-			if !is_there {
-				movie.Title = entry.Name()
-				movie.Path = fullpath
-				jobs <- movie
-			}
-		}
-	}
-}
-
 func GetAllPath() Response {
 	home := GetHomeDir()
-	fullpath := filepath.Join(home, "Videos")
-	jobs := make(chan Episode, 1000)
+	episodes := make(chan Episode, 10000)
+	var res Response
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go walkOptimized(home, episodes, &wg)
+
 	go func() {
-		for path := range jobs {
-			mu.Lock()
-			videoPath = append(videoPath, path)
-			mu.Unlock()
-		}
+		wg.Wait()
+		close(episodes)
 	}()
 
-	fmt.Println("scanning from: ", fullpath)
-	wg.Add(1)
-	go scanDir(fullpath, jobs)
-
-	wg.Wait()
-	close(jobs)
-
-	var allMovies []string
-	for _, movie := range videoPath {
-		var is_there = false
-		for _, enregistred := range allMovies {
-			if movie.Title == enregistred {
-				is_there = true
-			}
-		}
-		if !is_there {
-			allMovies = append(allMovies, movie.Title)
-		}
+	group := make(map[string][]string)
+	for ep := range episodes {
+		group[ep.Title] = append(group[ep.Title], ep.Path)
 	}
 
-	var res Response
-	for _,saga := range allMovies {
-		var tempSaga Serie
-		tempSaga.Title = saga
-		for _, movie := range videoPath {
-			if movie.Title == saga {
-				tempSaga.Ep_list = append(tempSaga.Ep_list, movie.Path)
-			}
+	for title, paths := range group {
+		if len(title) < 2 {
+			continue
 		}
-		res.List = append(res.List, tempSaga)
+		lowerTitle := strings.ToLower(title)
+		if strings.Contains(lowerTitle, "mp4") || strings.Contains(lowerTitle, "image_room") {
+			continue
+		}
+		serie := Serie{Title: title, Ep_list: paths}
+		res.List = append(res.List, serie)
 	}
 
 	return res
